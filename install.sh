8d3bbb9a2cf2c3b8d9c0527f2a306922217e7828
chris-blackburn <chrisnblackburn@gmail.com>
2019-10-01 22:17:37 -0400

    Bug with install script looping links

--- install.sh
+++ install.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-set -e
 
 echo "Initializing Submodules"
 git submodule init && git submodule update
 
 echo "Linking files to appropriate locations"
-ln -s $PWD/.bash_profile ~/.bash_profile
-ln -s $PWD/.bashrc ~/.bashrc
-ln -s $PWD/.vim ~/.vim
-ln -s $PWD/.tmux.conf ~/.tmux.conf
-ln -s $PWD/.tmuxline.sh ~/.tmuxline.sh
-ln -s $PWD/.promptline.sh ~/.promptline.sh
+ln -s "$PWD/.bash_profile" ~/.bash_profile
+ln -s "$PWD/.bashrc" ~/.bashrc
+ln -s "$PWD/.vim" ~/.vim
+ln -s "$PWD/.tmux.conf" ~/.tmux.conf
+ln -s "$PWD/.tmuxline.sh" ~/.tmuxline.sh
+ln -s "$PWD/.promptline.sh" ~/.promptline.sh
 mkdir -p ~/.config/fish
-ln -s $PWD/.config.fish ~/.config/fish/config.fish
+ln -s "$PWD/.config.fish" ~/.config/fish/config.fish
 
 echo "Done"
