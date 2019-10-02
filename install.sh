405a7342e24eb437dca1458d3e8053d2fca0c8df
chris-blackburn <chrisnblackburn@gmail.com>
2019-10-02 00:19:10 -0400

    Fonts and updated install

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
+set -e
 
 echo "Initializing Submodules"
 git submodule init && git submodule update
+
+echo "Backing up original files"
+for f in ~/.bash_profile ~/.bashrc ~/.vim ~/.tmux.conf ~/.tmuxline.sh \
+	~/.promptline.sh ~/.config/fish/config.fish; do
+	mv "$f" "$f".dotbak
+done
 
 echo "Linking files to appropriate locations"
 ln -s "$PWD/.bash_profile" ~/.bash_profile
