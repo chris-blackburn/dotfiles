901461b5321a06e125af477fffccf0d4caccb7ad
Christopher Blackburn <krizboy@vt.edu>
2021-01-28 19:45:26 -0500

    updates, generate tmuxline/promptline on install

--- .vim/templates/template.h
+++ .vim/templates/template.h
@@ -1,6 +1,15 @@
+/**
+ * HEADER
+ *
+ * Description
+ *
+ * Author: Christopher Blackburn <krizboy@vt.edu>
+ * Date: 1/1/1977
+ */
+
 #ifndef __HEADER_H_
 #define __HEADER_H_
 
 
 
-#endif
+#endif /* __HEADER_H_ */
--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ if [ ! -d "./dotbak" ]; then
 fi
 
 echo "Backing up original files"
-for f in ~/.bash_profile ~/.bashrc ~/.vim ~/.ctags ~/.tmux.conf ~/.tmuxline.sh \
+for f in ~/.bash_profile ~/.bashrc ~/.vim ~/.vimrc ~/.ctags ~/.tmux.conf ~/.tmuxline.sh \
 	~/.promptline.sh ~/.config/fish/config.fish ~/.gitignore_global; do
 	mv "$f" "./dotbak/"$(basename -- "$f")
 done
@@ -22,21 +22,19 @@ ln -s "$PWD/.vim" ~/.vim
 ln -s "$PWD/.ctags" ~/.ctags
 ln -s "$PWD/.tmux.conf" ~/.tmux.conf
 ln -s "$PWD/.tmux" ~/.tmux
-ln -s "$PWD/.tmuxline.sh" ~/.tmuxline.sh
-ln -s "$PWD/.promptline.sh" ~/.promptline.sh
 mkdir -p ~/.config/fish
 ln -s "$PWD/.config.fish" ~/.config/fish/config.fish
 ln -s "$PWD/.gitignore_global" ~/.gitignore_global
+
+# Create promptline and tmuxline files for nice styling (based on vimrc theme)
+vim "+PromptlineSnapshot! ~/.promptline.sh" "+qall"
+vim "+Tmuxline lightline" "+TmuxlineSnapshot! ~/.tmuxline.sh" "+qall"
 
 echo "Updating git global config to use global .gitignore"
 git config --global core.excludesfile ~/.gitignore_global
 
 echo "Making GDB cool"
-if test -f ~/.gdbinit; then
-	echo "GDB already cool"
-else
-	wget -P ~ https://git.io/.gdbinit
-fi
+wget -qNP ~ https://git.io/.gdbinit
 
 echo "Done"
 echo "Make sure to go to https://github.com/powerline/fonts.git to get some good fonts"
